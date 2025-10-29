927012136ad35119d6ead1d244d88af31a713c58
madhan6374118410-blip <madhan6374118410@gmail.com>
2025-10-29 01:30:15 -0700

    Add files via upload

--- madhan01.cpp
+++ madhan01.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
-int main() {
-    int num1 = 12;
-    int num2 = 13;
-
-    if (num1 > num2) {
-        std::cout << num1 << " is the greatest" << std::endl;
-    } else if (num2 > num1) {
-        std::cout << num2 << " is the greatest" << std::endl;
-    } else {
-        std::cout << "Both numbers are equal" << std::endl;
-    }
-    int greatest = (num1 > num2) ? num1 : num2;
-    std::cout << "The greatest number is: " << greatest << std::endl;
-
-    return 0;
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d\n",a+b);
+	printf("%d\n",a-b);
+	printf("%d\n",a*b);
+	printf("%d\n",a/b);
+	printf("%d\n",a%b);
+	return 0;
 }
